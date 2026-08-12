462a5601f5e338f65c2bcc9663d7de4d0f283bf6
camicastilloo <camilacastillo1004@hotmail.com>
2026-08-12 16:34:18 -0300

    Cambio catorce

--- hola_mundo.py
+++ hola_mundo.py
@@ -1,2 +1,2 @@
 print("Hola Mundo")
-print("Uno, Dos, Tres, Cuatro, Cinco, Seis, Siete, Ocho, Nueve, Diez, Once, Doce, Trece")
+print("Uno, Dos, Tres, Cuatro, Cinco, Seis, Siete, Ocho, Nueve, Diez, Once, Doce, Trece, Catorce")
